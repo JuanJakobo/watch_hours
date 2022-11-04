aefad02b0e8042f1d1a409cb4edba0e278b92246
JuanJakobo <github@johannsn.com>
2022-11-04 11:55:08 +0100

    save log into sqlite

--- main.py
+++ main.py
@@ -1,15 +1,51 @@
 from wmctrl import Desktop, Window
 from datetime import datetime
 import time
+import sqlite3
+import sys
+from sys import argv
+import os
+from notify import notification
 
-counter = 0
-while(True):
-    out = str(datetime.now().strftime('%Y-%m-%d;%H:%M')) + ';' + str(counter)
-    activeDesktop = Desktop.get_active().num
-    for i in Window.list():
-        if(i.desktop == activeDesktop):
-            out = out + ';' + i.wm_name
-            #TODO tmux window name
-    print(out)
-    time.sleep(60)
-    counter += counter
+def main():
+    #check if arguments are supplied
+    path = ""
+    if len(argv) >= 2:
+        for arg in argv:
+            if arg == "-f":
+                write_usage_to_db(path)
+
+def write_usage_to_db(path):
+    try:
+        connection = sqlite3.connect(path)
+        counter = 0
+        while(True):
+            current_window = Window.get_active()
+            if current_window is not None:
+                window_class = currentWindow.wm_class
+                if window_class == "Alacritty.Alacritty":
+                    #TODO tmux window name
+                    window_name = "Code"
+                else:
+                    window_name = currentWindow.wm_name
+            else:
+                window_class = "Empty.Empty"
+                window_name = "Empty"
+            cursor = connection.cursor()
+            cursor.execute("INSERT INTO USAGE VALUES (?,?,?)", (datetime.now(), window_class, window_name))
+            connection.commit()
+            counter += counter
+            if counter % 60 == 0:
+                notification('Screentime',message=f"uptime: {counter/60} h",app_name='usage')
+            time.sleep(60)
+            counter =+ 1
+    except KeyboardInterrupt:
+        print('Closing')
+        connection.close()
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(1)
+
+if __name__ == '__main__':
+    main()
