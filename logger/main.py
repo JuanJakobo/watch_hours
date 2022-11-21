185b25659da1173ffde6a73b78bea1f6a397488b
JuanJakobo <github@johannsn.com>
2022-11-21 09:29:17 +0100

    Restructure logger

--- logger/main.py
+++ logger/main.py
@@ -5,7 +5,7 @@ The values can be analyized using this program also.
 Usage:
   usage.py write <path> [--timer=<num>] [--verbose]
   usage.py log <path> [--size=<num>]
-  usage.py print <path> <report> [--day=<date>] [--days=<num>] [--className=<str>]
+  usage.py print <path> [--days=<num>]
   usage.py (-h | --help)
   usage.py --version
 
@@ -18,11 +18,7 @@ Options:
   log
     --size=<num>  Count of entries [default: 10].
   print
-    <report> Available reports are AvgPerWeekday, DailyUsage, DetailedClass, PreviousDaysUsage,\
-            ProgrammUsage [default: AvgPerWeekday]
-    --day=<date>  Date to report on [default: 2022-11-07]
-    --days=<num>  Count of days to report on for PreviousDaysUsage [default: 1].
-    --className=<num>  Name of class to detail [default: firefox].
+    --days=<num>  Count of days to report [default: 10].
 """
 from datetime import datetime
 import sqlite3
@@ -45,16 +41,7 @@ def main():
         write_usage_to_db(path=arguments['<path>'],\
                 timer=int(arguments['--timer']), verbose=arguments['--verbose'])
     elif arguments['print']:
-        if arguments['<report>'] == "AvgPerWeekday":
-            draw_average_usage_per_weekday(path=arguments['<path>'])
-        elif arguments['<report>'] == "DailyUsage":
-            draw_daily_usage(path=arguments['<path>'], day=arguments['--day'])
-        elif arguments['<report>'] == "DetailedClass":
-            draw_detailed_class_usage(path=arguments['<path>'], class_name=arguments['--className'])
-        elif arguments['<report>'] == "PreviousDaysUsage":
             draw_previous_days_usage(path=arguments['<path>'],days=arguments['--days'])
-        elif arguments['<report>'] == "ProgrammUsage":
-            draw_program_usage(path=arguments['<path>'])
 
 def create_database(path):
     """Opens the DB and creates a new table if not existend"""
@@ -152,45 +139,6 @@ def draw_log(path, limit):
         print(f"{row[0]} | {row[1]:<20} | {row[2]}")
     connection.close()
 
-def draw_average_usage_per_weekday(path):
-    """Draws the average usage per weekday to the terminal"""
-    connection = sqlite3.connect(path)
-    cursor = connection.cursor()
-    #TODO avrg
-    rows = cursor.execute("SELECT STRFTIME('%w',date,'unixepoch') AS day, COUNT(*) AS time \
-            FROM usage GROUP BY day")
-
-    day_names = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
-    for row in rows:
-        print(f"{day_names[int(row[0])-1]:<9} {format_time(row[1]):<3}")
-    connection.close()
-
-def draw_program_usage(path):
-    """Draws the usage seperated per program to the terminal"""
-    connection = sqlite3.connect(path)
-    cursor = connection.cursor()
-    rows = cursor.execute("SELECT ws.name, Count(u.windowClassId) \
-            AS usage FROM usage AS u JOIN windowClasses AS ws ON u.windowClassId = ws.id \
-            GROUP BY u.windowClassId ORDER BY usage DESC")
-
-    for row in rows:
-        print(f"{row[0]:<25} {format_time(row[1]):<3}")
-    connection.close()
-
-def draw_daily_usage(path, day):
-    """Draws the usage for today grouped by hour to the terminal"""
-    connection = sqlite3.connect(path)
-    cursor = connection.cursor()
-    rows = cursor.execute("SELECT strftime('%H',date,'unixepoch','localtime') AS time, COUNT(*) \
-            FROM usage WHERE DATE(date,'unixepoch','localtime') IS ? GROUP BY time \
-            ORDER BY time ASC", (day,))
-    for row in rows:
-        print(f"{row[0]:<2} ({row[1]:<2} min) ",end="")
-        for _ in range(row[1]):
-            print("#",end="")
-        print()
-    connection.close()
-
 def draw_previous_days_usage(path, days):
     """Draws the usage for the last 'days' including today to the terminal"""
     connection = sqlite3.connect(path)
@@ -201,27 +149,6 @@ def draw_previous_days_usage(path, days):
             GROUP BY time ORDER BY time ASC", (days,))
     for row in rows:
         print(f"{row[0]:<2} {format_time(row[1]):<3}")
-    connection.close()
-
-def draw_detailed_class_usage(path, class_name):
-    """Draws the detailed usage of a certain program to the terminal"""
-    connection = sqlite3.connect(path)
-    cursor = connection.cursor()
-    window_class_name = "%" + class_name + "%"
-    rows = cursor.execute("SELECT focusedWindowName, COUNT(focusedWindowName) \
-            AS usage FROM usage JOIN windowClasses ON usage.windowClassId = windowClasses.id  WHERE windowClasses.name LIKE ? \
-            GROUP BY focusedWindowName ORDER BY usage ASC", (window_class_name,))
-
-    #TODO define seperator, add up items
-    #seperator is - or | or page name is at beginning
-    for row in rows:
-        test = row[0].split('—')[0] #.split()[-1]
-        if '|' in test:
-            test = test.split('|')[-1].strip()
-        if '-' in test:
-            test = test.split('-')[-1].strip()
-
-        print(f"{test:<50} {format_time(row[1]):<3}")
     connection.close()
 
 if __name__ == '__main__':
